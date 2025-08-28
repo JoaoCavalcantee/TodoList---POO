8ce65f9cfc11e747b8e539db1cfa2bed14823fe3
joao.lborges <joaocavalcantt@outlook.com>
2025-08-28 11:11:11 -0300

    Capitulo 1.2

--- src/main/java/ucb/aplicativo/cli/Main.java
+++ src/main/java/ucb/aplicativo/cli/Main.java
@@ -2,6 +2,11 @@ package ucb.aplicativo.cli;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("*****************************");
+        System.out.println("*Bem-Vindo Lista de Tarefas!*");
+        System.out.println("*****************************");
 
+        System.out.println("1 - Para Adicionar Nova Tarefa");
+        System.out.println("2 - Para Visualizar as Tarefas");
     }
 }
