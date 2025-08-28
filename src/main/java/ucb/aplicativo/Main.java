c1999d690aef53b01b3258d373a956e2ac8c45ad
joao.lborges <joaocavalcantt@outlook.com>
2025-08-28 10:45:10 -0300

    Capitulo 1

--- src/main/java/ucb/aplicativo/Main.java
+++
@@ -1,7 +0,0 @@
-package ucb.aplicativo;
-
-public class Main{
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/ucb/aplicativo/model/Tarefa.java
@@ -0,0 +1,58 @@
+package ucb.aplicativo.model;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Tarefa {
+
+    private Long  id;
+    private String titulo;
+    private String descricao;
+    private boolean completa;
+    private LocalDateTime dataAgora;
+
+    public Tarefa(Long id, boolean completa) {
+        this.id = id;
+        this.completa = completa;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isCompleta() {
+        return completa;
+    }
+
+    public LocalDateTime getDataAgora() {
+        return dataAgora;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setCompleta(boolean completa) {
+        this.completa = completa;
+    }
+
+    public void setDataAgora(LocalDateTime dataAgora) {
+        this.dataAgora = dataAgora;
+    }
+}
